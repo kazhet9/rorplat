939d9b5f63c82634752b4e0752fd94da5824c0f1
Karen Chizón <karen.chizon@gmail.com>
2018-10-21 15:56:37 -0500

    enchulado

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
-  root 'pages#index'
+  root 'articles#index'
+  # root 'pages#index'
 
   # get 'pages/test'
   get 'test' => 'pages#test'
